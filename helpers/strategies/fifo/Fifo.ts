e58d5610a43cc1761e21a004c9dc676c85f1be87
aminesayagh <aminesayagh1997@gmail.com>
2022-04-27 09:42:24 +0000

    import

--- helpers/strategies/fifo/Fifo.ts
+++ helpers/strategies/fifo/Fifo.ts
@@ -1,18 +1,24 @@
 import File, { STRUCTURE } from '../../structure/Structure';
 
+export const listMethods = {
+    generateOrdinaryList: () => {
+        if(!this) throw new Error('this method is used as a weak coupling');
+        // @ts-ignore
+        this._listSorted = [...this._listCases].sort();
+    }
+} 
 export default class Fifo extends File<number>{
+    public _listCases: number[];
     _listSorted: number[];
+    private generateOrdinaryList;
     constructor(){
         super({ type:STRUCTURE.FILE });
         this._listCases = null;
         this._listSorted = null;
-
+        this.generateOrdinaryList = listMethods.generateOrdinaryList;
         this.generateOrdinaryList();
     }
-    public generateOrdinaryList(){
-        if(!this._listCases.length) throw new Error("Error in ording list, your list is empty");
-        this._listSorted = this._listCases.sort();
-    }
+    
     public calculateRendu(){
         
     }
