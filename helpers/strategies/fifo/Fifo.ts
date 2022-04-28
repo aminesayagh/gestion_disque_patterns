4e785be4a47da6261c06a61c9861283ef5f68c9d
aminesayagh <aminesayagh1997@gmail.com>
2022-04-28 05:23:54 +0000

    fifo update

--- helpers/strategies/fifo/Fifo.ts
+++ helpers/strategies/fifo/Fifo.ts
@@ -6,20 +6,32 @@ export const listMethods = {
         // @ts-ignore
         this._listSorted = [...this._listCases].sort();
     }
-} 
+}
+
+type TypeCase = number;
+
 export default class Fifo extends File<number>{
-    public _listCases: number[];
-    _listSorted: number[];
+    public _listCases: TypeCase[];
+    _listSorted: TypeCase[];
+    private _head: TypeCase;
+    private _listValuesToCalcul;
+
     private generateOrdinaryList;
+
     constructor(){
         super({ type:STRUCTURE.FILE });
         this._listCases = null;
         this._listSorted = null;
         this.generateOrdinaryList = listMethods.generateOrdinaryList;
+
         this.generateOrdinaryList();
     }
-    
+    set head(value: TypeCase){
+        this._head = value;
+    }
     public calculateRendu(){
+        this._listValuesToCalcul = new File<{ firstValue: TypeCase, secondaryValue: TypeCase }>({ type: STRUCTURE.FILE });
         
+        this._listValuesToCalcul.postCase({ firstValue: this._head, secondaryValue: this.pullCase });
     }
 }
